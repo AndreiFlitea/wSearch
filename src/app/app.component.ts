a0486be3fa654a406a0e83876a7b2346c3a710ce
Andrei Flitea <fliteaandrei@gmail.com>
2023-02-10 13:50:39 +0200

    https://wsearch-peach-two.vercel.app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { WikipediaService } from './wikipedia.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'wSearch';
+  constructor(private wikipedia: WikipediaService) {}
+  pages = [];
+  receiveTerm(term: string) {
+    this.wikipedia.searchTerm(term).subscribe((response) => {
+      this.pages = response;
+    });
+  }
 }
