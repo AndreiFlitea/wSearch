7c9b2435e989effae00b8174c01fbf0f4fb7dabf
Andrei Flitea <fliteaandrei@gmail.com>
2023-02-10 14:06:11 +0200

    https://wsearch-peach-two.vercel.app/

---
+++ src/app/search-bar/search-bar.component.ts
@@ -0,0 +1,20 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-search-bar',
+  templateUrl: './search-bar.component.html',
+  styleUrls: ['./search-bar.component.css'],
+})
+export class SearchBarComponent {
+  @Output() submitted = new EventEmitter<string>();
+  term = '';
+
+  onInput(event: Event): void {
+    this.term = (event.target as HTMLInputElement).value;
+  }
+
+  onFormSubmit(event: any) {
+    event.preventDefault();
+    this.submitted.emit(this.term);
+  }
+}
